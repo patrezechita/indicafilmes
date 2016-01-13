3c1e5dc29c71019df1424c4764f6aa1b3c0a9253
Patreze <patrezechita@outlook.com>
2016-01-13 20:56:19 -0300

    teste da nova api
    eu realmente espero que resolva o problema do cross domain

--- consultas.js
+++ consultas.js
@@ -1,30 +1,61 @@
 // recebe um nome para pesquisar e retorna JSON com os filmes que achar
 function pesquisaFilmeNome(nomeFilme) {
 
-    document.getElementById("divPesquisa").innerHTML = "carregando";
-    $.ajax({
-        url: 'http://www.omdbapi.com/?s=' + nomeFilme + '&r=JSON&type=movie',
-        type: 'get',
-        dataType: 'jsonp',
 
-        success: function(response, textStatus, jqXHR) {
 
-            for (var i = 0; i < response.Search.length; i++) {
-                document.getElementById("divPesquisa").innerHTML = "";
-                desenha_IMDb_DIV(response.Search[i].imdbID, "divPesquisa");
-                //console.log(response.Search[i].Title + " " + response.Search[i].imdbID);
-                //document.getElementById("demo").innerHTML += response.Search[i].Title + " ";
+
+    var xmlhttp = new XMLHttpRequest();
+    var url = "http://api.themoviedb.org/3/search/movie?api_key=" + geraToken() + "&query=" + nomeFilme + "'";
+
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            //myFunction(xmlhttp.responseText);
+            var myArr = JSON.parse(xmlhttp.responseText);
+            //console.log(myArr.results[2]);
+
+
+                    for (var i = 0; i < myArr.results.length; i++) {
+
+
+                        if (myArr.results[i].poster_path === null) {
+                            myArr.results[i].poster_path = "semcapa.png";
+                        }
+                        else{
+                            myArr.results[i].poster_path = "http://image.tmdb.org/t/p/w500" + myArr.results[i].poster_path;
+                        }
+            //document.getElementById("divPesquisa").innerHTML = "";
+            //desenha_IMDb_DIV(response.Search[i].imdbID, "divPesquisa");
+            //console.log(response.Search[i].Title + " " + response.Search[i].imdbID);
+            //document.getElementById("demo").innerHTML += response.Search[i].Title + " ";
+            console.log(myArr.results[i]);
+            document.getElementById("divPesquisa").innerHTML += "<div id='filmeCapaCompleta'><img src='" + myArr.results[i].poster_path + "'/>" +
+                "<br><strong>" + myArr.results[i].original_title + "</strong>" +
+                "<ul><li>Ano:" + myArr.results[i].release_date + "</li>" +
+                "<li>Genero:" + "" + "</li>" +
+                "<li>Sinopse:" + myArr.results[i].overview + "</li>" +
+                "<li>nota:" + myArr.results[i].vote_average + "</li>" +
+                "<li>imdb:" + "" + "</li></ul></div>";
+
             }
 
 
-        },
 
-        error: function(jqXHR, textStatus, errorThrown) {
-            // tratamento de erro
+
+
+
+
         }
-    });
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+
 }
 
+
+function geraToken(){
+    return "871050ddbdac415dbd122a25e60e7208";
+}
 
 //desenha_IMDb_DIV("tt1424432", "divPesquisa");
 
@@ -157,34 +188,16 @@ function louco() {
 
 
 
-$.ajax({
-//  data: parms,
-  url: 'http://ia.media-imdb.com/images/M/MV5BMTcwNzk2MjQyNl5BMl5BanBnXkFtZTcwMjA0MjEzMQ@@._V1_SX300.jpg',
-  type: 'get',
-  dataType: 'image/jpeg',
-  success: function (response, textStatus, jqXHR) {
-    // Here your code
-    console.log(response);
-    $("divPesquisa").append(response);
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-  }
-});
-
-
-
-
-
-function image() {
-    var img = document.createElement("IMG");
-    img.src = "http://ia.media-imdb.com/images/M/MV5BNDk2NTk1NzQxN15BMl5BanBnXkFtZTYwMDMyMTg5._V1_SX300.jpg";
-    $('#image').html(img);
-}
 
 
 
 
 
 
+
+
+
+
+//API Key: 871050ddbdac415dbd122a25e60e7208
 
 //lembrar de aleatorizar a porra do token
